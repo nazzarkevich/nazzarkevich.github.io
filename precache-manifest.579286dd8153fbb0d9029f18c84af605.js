f0a83f0e2201d01573d9bcbd131f16f3a18a28e1
nazzarkevich <nazzik.3@gmail.com>
2018-12-27 23:45:42 +0200

    Updates

---
+++ precache-manifest.579286dd8153fbb0d9029f18c84af605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4b8adad088ed44bc4cb",
+    "url": "/venue-dnd/static/js/runtime~main.f4b8adad.js"
+  },
+  {
+    "revision": "b95a6f1f6a60c90e1682",
+    "url": "/venue-dnd/static/js/main.b95a6f1f.chunk.js"
+  },
+  {
+    "revision": "a0ab7c85f8c3c6bdf613",
+    "url": "/venue-dnd/static/js/1.a0ab7c85.chunk.js"
+  },
+  {
+    "revision": "b95a6f1f6a60c90e1682",
+    "url": "/venue-dnd/static/css/main.4a080b22.chunk.css"
+  },
+  {
+    "revision": "5693eaf1560358c82c859a0d559d9c8b",
+    "url": "/venue-dnd/index.html"
+  }
+];
